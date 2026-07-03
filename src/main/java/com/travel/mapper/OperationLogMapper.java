47d286840bafb86cabdf36b3071d762ae6c5a838
gzylh <1814675884@qq.com>
2026-07-03 16:16:42 +0800

    删除了管理员日志相应模块

--- src/main/java/com/travel/mapper/OperationLogMapper.java
+++
@@ -1,7 +0,0 @@
-package com.travel.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.travel.entity.OperationLog;
-
-public interface OperationLogMapper extends BaseMapper<OperationLog> {
-}
